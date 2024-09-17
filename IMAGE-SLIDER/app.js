cf58c7803439d4f92502d5d6854986f9ab3c5dc7
SairaBatool123 <sairabatool787@gmail.com>
2024-09-17 09:17:50 -0700

    Done

--- IMAGE-SLIDER/app.js
+++ IMAGE-SLIDER/app.js
@@ -1,6 +1,26 @@
 const slide = document.querySelectorAll(".slider")
+// console.log(slide);
 
-var count = 0
-slide.forEach(()=>{
 
+var counter = 0
+slide.forEach((slide, index) => {
+    // slide.computedStyleMap.bottom = `${index * 100}`
+    slide.style.left = `${index * 100}%`
 })
+const goNext = () => {
+    counter++
+    slideImg()
+}
+const goPrevious = () => {
+    counter--
+    slideImg()
+}
+
+const slideImg = () => {
+    slide.forEach(
+        (slide) => {
+            //  slide.style.transform = `translateY(-$(counter*100)%)`
+            slide.style.transform = `translateX(-${counter*100}%)`
+        }
+    )
+}
